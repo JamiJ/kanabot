e03aebd3a99ca6ea11177e9c30d219695fda9252
JamiJ <35563202+JamiJ@users.noreply.github.com>
2020-12-02 18:17:42 +0200

    database handling

--- lib/db/db.py
+++ lib/db/db.py
@@ -1,5 +1,7 @@
+#import sqlite3
 from os.path import isfile
-from sqlite import connect
+from sqlite3 import connect
+from apscheduler.triggers.cron import CronTrigger
 
 DB_PATH = "./data/db/database.db"
 BUILD_PATH = "./data/db/build.sql"
@@ -20,11 +22,14 @@ def build():
 	if isfile(BUILD_PATH):
 		scriptexec(BUILD_PATH)
 
-
-
 def commit():
+	print("commiting...")
 	cxn.commit()
 
+def autosave(sched):
+	sched.add_job(commit, CronTrigger(second=0))
+#second=0 = every minute
+#minute=0 = every hour	
 
 def close():
 	cxn.close()
