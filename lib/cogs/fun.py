0f63a5a07025c197492ef2fbb80ee82ff9875549
JamiJ <35563202+JamiJ@users.noreply.github.com>
2020-12-08 17:45:56 +0200

    fixes and typos

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -2,6 +2,7 @@ from random import choice, randint
 from typing import Optional
 
 from discord import Member
+from discord.errors import HttpException
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -15,18 +16,18 @@ class Fun(Cog):
 	async def say_hello(self, ctx):
 		await ctx.send(f"[{choice(('Hello', 'Hi', 'Hey'))} {ctx.author.mention}!")
 
-	@command(name="dice", aliases=["roll"])
-	async def roll_dice(self, ctx, die_string: str):
-		dice, value = (int(term) for term in die_string.split("d"))
-		rolls = [randint(1, value) for i in range(dice)]
-
-		await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum.(rolls)}")
+#	@command(name="dice", aliases=["roll"])
+#	async def roll_dice(self, ctx, die_string: str):
+#		dice, value = (int(term) for term in die_string.split("d"))
+#		rolls = [randint(1, value) for i in range(dice)]
+#
+#		await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum.(rolls)}")
 
 	@command(name="slap", aliases=["hit"])
 	async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "no reason"):
 		await ctx.send(f"{ctx.author.display_name} slapped {member.mention} {reason}!")
 
-	@comnmand(name="echo", aliases=["say"])
+	@command(name="echo", aliases=["say"])
 	async def echo_message(self, ctx, *, message):
 		await ctx.message.delete()
 		await ctx.send(message)
